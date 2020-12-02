ab53ed67b6fb0c479bd14d4adb1bd5d49d0ebe3d
AdilsonAr <cycles.250@gmail.com>
2020-12-02 14:41:00 -0600

    elimine elementos innecesarios

--- src/test/java/com/atc/ContabilidadApplicationTests.java
+++ src/test/java/com/atc/ContabilidadApplicationTests.java
@@ -1,34 +1,13 @@
 package com.atc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.atc.model.Login;
-import com.atc.model.User;
-import com.atc.service.LoginService;
-import com.atc.service.UserService;
 
 @SpringBootTest
 class ContabilidadApplicationTests {
-	
-	@Autowired
-	BCryptPasswordEncoder encoder;
-	@Autowired
-	LoginService loginService;
-	@Autowired
-	UserService userService;
 	@Test
 	void contextLoads() {
-		User user=new User("Adilson Arbuez","ognyom@gmail.com");
-		userService.create(user);
-		Login login=new Login("Adilson",encoder.encode("armath"),"ADMIN",user);
-		Login me=loginService.create(login);
-		assertEquals(me.getLogin(), login.getLogin());
-		assertEquals(me.getPassword(), login.getPassword());
+		
 	}
 
 }
